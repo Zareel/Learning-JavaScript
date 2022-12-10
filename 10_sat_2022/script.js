e9f37821f4c044354273cfc21d0c8155d9b8dc0b
Zareel Kalam <zareelbwd@gmail.com>
2022-12-10 22:12:06 +0530

    first commit

---
+++ 10_sat_2022/script.js
@@ -0,0 +1,35 @@
+/* 
+y = 30;
+console.log(y);
+let y = 20;
+*/
+
+/*
+let age = 16;
+if (age >= 18) {
+  console.log("you are allowed inside the park");
+}else{
+    console.log("Out of the park");
+}
+*/
+/*
+let gender = "female";
+if (genter == "female"){
+    console.log("you are allowed")
+}else{
+    console.log("not allowed");
+}
+*/
+/*
+let standard = 6;
+if (standard == 1) {
+  console.log("room 2");
+} else if (standard == 2) {
+  console.log("room 3");
+} else {
+  console.log("room 5");
+}
+*/
+
+let name = "zareel";
+console.log(typeof name);
